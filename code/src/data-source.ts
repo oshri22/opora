0d615bcc93861fc75171c28907c80a081248ab32
oshri22 <oshritaub@gmail.com>
2022-05-11 17:18:41 +0300

    add al the entities to connector

--- code/src/data-source.ts
+++ code/src/data-source.ts
@@ -1,8 +1,19 @@
 import "reflect-metadata"
 import { MYSQL } from "./config"
-
 import { DataSource } from "typeorm"
 import { Circuits } from "./entity/circuits"
+import { Constructor_results } from "./entity/constructor_results"
+import { Constructor_standings } from "./entity/constructor_standings"
+import { Constructors } from "./entity/constructors"
+import { Driver_standings } from "./entity/driver_standings"
+import { Drivers } from "./entity/drivers"
+import { Lap_times } from "./entity/lap_times"
+import { Pit_stops } from "./entity/pit_stops"
+import { Qualifying } from "./entity/qualifying"
+import { Races } from "./entity/races"
+import { Results } from "./entity/results"
+import { Seasons } from "./entity/seasons"
+import { Status } from "./entity/status"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -13,7 +24,7 @@ export const AppDataSource = new DataSource({
     database: MYSQL.data_base,
     synchronize: true,
     logging: false,
-    entities: [Circuits],
+    entities: [Circuits, Constructor_results, Constructor_standings, Constructors, Driver_standings, Drivers, Lap_times, Pit_stops, Qualifying, Races, Results, Seasons, Status],
     migrations: [],
     subscribers: [],
 })
